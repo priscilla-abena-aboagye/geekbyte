4276e9984dedc93df145b0104b2981662980a96a
Abena <priscillaabenaaboagye@gmail.com>
2025-11-19 10:49:20 +0000

    trials for list

---
+++ list_trials.py
@@ -0,0 +1,11 @@
+fruits = ["apple", "banana", "cherry", "mango"]
+numbers = [1, 2, 3, 4, 5]
+mix = [23, 4, "banana", 9.6]
+
+# for num in numbers:
+
+#     print(num)
+
+
+single_num = [num ** 2 for num in numbers]
+print(single_num)
