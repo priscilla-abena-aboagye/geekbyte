db1a01e32306bee338d11d4ba2e46836b3a3b11c
Abena <priscillaabenaaboagye@gmail.com>
2025-12-18 16:31:04 +0000

    function to add members

--- smart_community.py
+++ smart_community.py
@@ -1,6 +1,37 @@
 members = []
 member_ids = set()
 
+# add members (heads only)
+def add_members():
+    print("Add Members")
+    print("-------------------------------")
+    member_id = input("Member ID: ") # takes id
+    # prevents duplicates
+    if member_id in member_ids:
+        print("This ID already exists")
+        return 
+    
+    member_name = input("Member name: ")
+    member_age = input(f"{member_name}'s age: ")
+
+    # validates the age
+    if not member_age.isdigit():
+        print(f"Sorry, {member_name}'s age must be a number.")
+        return 
+    
+    member_age = int(member_age) # typecasting the age
+    # member dictionary
+    member = {
+        "id": member_id,
+        "name": member_name,
+        "age": member_age
+    }
+
+    members.append(member) # adds it to the list
+    member_ids.add(member_id) # adds it to the set to prevent duplicates
+
+    print("Member added successfully")
+
 
 
 # Main Program
